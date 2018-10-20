b05491526f9577412e44279d75635676f636b28d
bobambrosius <bob.ambrosius@gmail.com>
2018-10-20 12:26:30 +0200

    first commit

---
+++ dbhelper.py
@@ -0,0 +1,43 @@
+import pymysql
+import dbconfig
+
+class DBHelper:
+    def connect(self, database="crimemap"):
+            return pymysql.connect(
+            host = 'localhost',
+            user = dbconfig.db_user,
+            passwd = dbconfig.db_password,
+            db = database,
+            port = 3307
+            )
+
+    def get_all_inputs(self):
+        conn = self.connect()
+        try:
+            query = "select description from crimes;"
+            with conn.cursor() as cursor:
+                cursor.execute(query)
+            return cursor.fetchall()
+        finally:
+            conn.close()
+    
+    def add_input(self, data):
+        conn = self.connect()
+        try:
+            query = "INSERT INTO crimes (description) values (%s);"
+            with conn.cursor() as cursor:
+                cursor.execute(query, data)
+                conn.commit()
+        finally:
+            conn.close()
+             
+    def clear_all(self):
+        conn = self.connect()
+        try:
+            query = "DELETE FROM crimes;"
+            with conn.cursor() as cursor:
+                cursor.execute(query)
+                conn.commit()
+        finally:
+            conn.close()
+        
